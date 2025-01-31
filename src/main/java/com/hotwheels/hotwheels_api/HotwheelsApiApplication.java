b6a5c6b67b50a806fcdd7fd31b12c1b8add7ccfe
Dariohg <rubendarioh99@gmail.com>
2025-01-31 02:29:31 -0600

    first commit

---
+++ src/main/java/com/hotwheels/hotwheels_api/HotwheelsApiApplication.java
@@ -0,0 +1,13 @@
+package com.hotwheels.hotwheels_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotwheelsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotwheelsApiApplication.class, args);
+	}
+
+}
